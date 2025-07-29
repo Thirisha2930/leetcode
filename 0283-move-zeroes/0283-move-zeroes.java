527163691f51996d6ea03ebe6685dcf3abe8edbd
Thirisha2930 <thirisha492@gmail.com>
2025-07-29 11:45:36 +0530

    Time: 2 ms (80.07%), Space: 46.4 MB (20.63%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zero=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                if(i!=zero){
+                    int temp=nums[zero];
+                    nums[zero]=nums[i];
+                    nums[i]=temp;
+                }
+                zero++;
+            }
+        }
+        
+    }
+}
